70304516f6e73f8d438744c41d0771fc3b96d443
Vibhansh Jain <122611356+Vibhanshjain@users.noreply.github.com>
2025-05-07 23:18:21 +0530

    Update sum.java

--- src/main/java/com/example2/sum.java
+++ src/main/java/com/example2/sum.java
@@ -1,23 +1,9 @@
 package com.example2;
-import java.util.Scanner;
 
 public class sum {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Taking input from user
-        System.out.print("Enter first number: ");
-        int num1 = scanner.nextInt();
-
-        System.out.print("Enter second number: ");
-        int num2 = scanner.nextInt();
-
-        // Calculate sum
-        int sum = num1 + num2;
-
-        // Display result
-        System.out.println("Sum: " + sum);
-
-        scanner.close();
+        int a=4,b=6;
+        System.out.println("Sum: " + (a + b));
+       
     }
 }
