98039b1cebf85a136335cb7cb258f99393e2f9f3
Vibhansh Jain <122611356+Vibhanshjain@users.noreply.github.com>
2025-05-07 23:22:36 +0530

    Update sum.java

--- src/main/java/com/example2/sum.java
+++ src/main/java/com/example2/sum.java
@@ -1,9 +1,13 @@
-package com.example2;
-
 public class sum {
     public static void main(String[] args) {
-        int a=4,b=6;
-        System.out.println("Sum: " + (a + b));
-       
+        if (args.length < 2) {
+            System.out.println("Please provide two numbers as arguments.");
+            return;
+        }
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = a + b;
+        System.out.println("The sum is: " + c);
     }
 }
